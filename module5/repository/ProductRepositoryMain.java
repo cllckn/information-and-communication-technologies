00d4dbfa6485e806c466d8f65941b60932c56077
cllckn <orcait.en@gmail.com>
2025-11-10 14:47:56 +0500

    Update ProductRepositoryMain.java

--- module5/repository/ProductRepositoryMain.java
+++ module5/repository/ProductRepositoryMain.java
@@ -4,8 +4,8 @@ public class ProductRepositoryMain {
 
     public static void main(String[] args) {
         // Swap implementations easily
-        //IProductRepository repository = new ProductPostgresqlImplementation();
-        IProductRepository repository = new ProductMongodbImplementation();
+        IProductRepository repository = new ProductPostgresqlImplementation();
+        //IProductRepository repository = new ProductMongodbImplementation();
 
         ClientService service = new ClientService(repository);
         service.run();
